ec0e3bf82fed43989c70aa2653fafe02cc6bd059
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-01 10:21:41 +0000

    Day 1 - Part 2 Completed

--- Advant-Of-Code-2022/Advant-Of-Code-2022/CalorieCounting.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/CalorieCounting.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace Advant_Of_Code_2022
 {
     internal class CalorieCounting
     {
-        public static void run()
+        private static List<int> GetCalorieList()
         {
             StreamReader reader = File.OpenText("Inputs/Day1.txt");
             string line;
@@ -20,12 +21,47 @@ namespace Advant_Of_Code_2022
                 {
                     calorieList.Add(total);
                     total = 0;
-                } else
+                }
+                else
                 {
                     total += int.Parse(line);
                 }
             }
+            return calorieList;
+        }
+
+        public static void Part1()
+        {
+            List<int> calorieList = GetCalorieList();
             Console.WriteLine(calorieList.Max());
+        }
+
+        public static void Part2()
+        {
+            List<int> calorieList = GetCalorieList();
+            const int topSize = 3;
+            int[] top = new int[topSize];
+            for (int i = 0; i < calorieList.Count; i++)
+            {
+                for (int topIndex = 0; topIndex < topSize; topIndex++)
+                {
+                    if (calorieList[i] > top[topIndex])
+                    {
+                        for (int innerTopIndex = topIndex + 1; innerTopIndex < topSize; innerTopIndex++)
+                        {
+                            top[innerTopIndex] = top[innerTopIndex - 1];
+                        }
+                        top[topIndex] = calorieList[i];
+                        break;
+                    }
+                }
+            }
+            int total = 0;
+            for (int i = 0; i< top.Length; i++)
+            {
+                total += top[i];
+            }
+            Console.WriteLine(total);
         }
     }
 }
--- Advant-Of-Code-2022/Advant-Of-Code-2022/Program.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/Program.cs
@@ -1,11 +1,54 @@
-﻿namespace Advant_Of_Code_2022
+﻿using System.Runtime.CompilerServices;
+
+namespace Advant_Of_Code_2022
 {
     internal class Program
     {
+        private static readonly Dictionary<string, Delegate[]> DayToFunction =
+            new()
+            {
+                { "Day1", new Delegate[]
+                {
+                    CalorieCounting.Part1,
+                    CalorieCounting.Part2
+                }
+            }
+        };
         static void Main(string[] args)
         {
             Console.WriteLine("Which day do you want to access?");
-            CalorieCounting.run();
+            while (true)
+            {
+                string? dayString = Console.ReadLine();
+                if (dayString == null)
+                {
+                    continue;
+                }
+                try
+                {
+                    int day = int.Parse(dayString);
+                    var result = DayToFunction.ElementAt(day - 1);
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Are you sure you want to select " + result.Key + "?");
+                            string? partString = Console.ReadLine();
+                            int part = int.Parse(partString);
+                            result.Value[part].DynamicInvoke();
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Unable to get the part " + dayString);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unable to get the day " + dayString);
+                }
+            }
         }
     }
 }
