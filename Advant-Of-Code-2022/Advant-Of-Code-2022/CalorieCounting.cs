ebe317e3bdc13f9c049cafc8be7ffe15aa6b2862
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-01 08:40:35 +0000

    Day 1 - Part 1 Complete

---
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/CalorieCounting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advant_Of_Code_2022
+{
+    internal class CalorieCounting
+    {
+        public static void run()
+        {
+            StreamReader reader = File.OpenText("Inputs/Day1.txt");
+            string line;
+            int total = 0;
+            List<int> calorieList = new();
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line == "")
+                {
+                    calorieList.Add(total);
+                    total = 0;
+                } else
+                {
+                    total += int.Parse(line);
+                }
+            }
+            Console.WriteLine(calorieList.Max());
+        }
+    }
+}
---
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Advant_Of_Code_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which day do you want to access?");
+            CalorieCounting.run();
+        }
+    }
+}
