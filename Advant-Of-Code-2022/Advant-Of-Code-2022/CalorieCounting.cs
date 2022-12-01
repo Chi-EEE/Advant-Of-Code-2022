080a78ec3423c5803098ee86bd9332cb2543c274
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-01 11:00:13 +0000

    Reduced Day 1 - Part 2 code lines

--- Advant-Of-Code-2022/Advant-Of-Code-2022/CalorieCounting.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/CalorieCounting.cs
@@ -39,29 +39,9 @@ namespace Advant_Of_Code_2022
         public static void Part2()
         {
             List<int> calorieList = GetCalorieList();
-            const int topSize = 3;
-            int[] top = new int[topSize];
-            for (int i = 0; i < calorieList.Count; i++)
-            {
-                for (int topIndex = 0; topIndex < topSize; topIndex++)
-                {
-                    if (calorieList[i] > top[topIndex])
-                    {
-                        for (int innerTopIndex = topIndex + 1; innerTopIndex < topSize; innerTopIndex++)
-                        {
-                            top[innerTopIndex] = top[innerTopIndex - 1];
-                        }
-                        top[topIndex] = calorieList[i];
-                        break;
-                    }
-                }
-            }
-            int total = 0;
-            for (int i = 0; i< top.Length; i++)
-            {
-                total += top[i];
-            }
-            Console.WriteLine(total);
+            IEnumerable<int> topThreeCalories =
+                calorieList.OrderByDescending(calorie => calorie).Take(3);
+            Console.WriteLine(topThreeCalories.Sum());
         }
     }
 }
