de628aeda189a72eb938bb6c5f82892448cf2bbe
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-01 10:41:28 +0000

    Updated Main Menu

--- Advant-Of-Code-2022/Advant-Of-Code-2022/Program.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/Program.cs
@@ -14,39 +14,58 @@ namespace Advant_Of_Code_2022
                 }
             }
         };
+        static void WaitForKey() {
+            Console.Write("\nPlease enter a key...");
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Which day do you want to access?");
             while (true)
             {
+                Console.WriteLine("Which day do you want to access?");
+                Console.WriteLine("There are " + DayToFunction.Count + " days in this Calendar.");
                 string? dayString = Console.ReadLine();
                 if (dayString == null)
                 {
+                    Console.Clear();
                     continue;
                 }
                 try
                 {
-                    int day = int.Parse(dayString);
-                    var result = DayToFunction.ElementAt(day - 1);
+                    int day = int.Parse(dayString) - 1;
+                    var result = DayToFunction.ElementAt(day);
                     while (true)
                     {
+                        Console.WriteLine("=====================================\nYou are selecting " + result.Key + ".");
+                        Console.WriteLine("There are " + result.Value.Length + " parts for this day.");
+                        Console.Write("Select between the days >");
+                        string? partString = Console.ReadLine();
                         try
                         {
-                            Console.WriteLine("Are you sure you want to select " + result.Key + "?");
-                            string? partString = Console.ReadLine();
-                            int part = int.Parse(partString);
+                            if (partString == null)
+                            {
+                                Console.Clear();
+                                continue;
+                            }
+                            int part = int.Parse(partString) - 1;
                             result.Value[part].DynamicInvoke();
+                            WaitForKey();
+                            Console.Clear();
                             break;
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine("Unable to get the part " + dayString);
+                            Console.WriteLine("Unable to get the part " + partString);
+                            WaitForKey();
+                            Console.Clear();
                         }
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine("Unable to get the day " + dayString);
+                    WaitForKey();
+                    Console.Clear();
                 }
             }
         }
