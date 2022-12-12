61328f1b6a6c803e4a106eea0ae7fb26d33d9e72
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-12 14:02:32 +0000

    Completed Day 5 - Part 1

--- Advant-Of-Code-2022/Advant-Of-Code-2022/Day5.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/Day5.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Advant_Of_Code_2022
@@ -12,26 +13,70 @@ namespace Advant_Of_Code_2022
         public static void Part1()
         {
             StreamReader reader = File.OpenText("Inputs/Day5.txt");
-            string line;
+            string line = reader.ReadLine();
+            int amountOfCrates = (line.Length + 1) / 4;
             List<Stack<char>> crateStacks = new();
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < amountOfCrates; i++)
             {
                 crateStacks.Add(new());
             }
-            while ((line = reader.ReadLine()) != null)
             {
-                if (line.Length == 0) // This is to separate the crates from the actions
+                List<Stack<char>> reversedCrateStacks = new();
+                for (int i = 0; i < amountOfCrates; i++)
                 {
-                    break;
+                    reversedCrateStacks.Add(new());
                 }
-                for (int i = 0; i < 9; i++)
+                do
                 {
-                    bool isValue = char.Parse(line.Substring(i + i * 3, 1)) == '[';
-                    if (isValue)
+                    if (line.Length == 0) // This is to separate the crates from the actions
                     {
-                        crateStacks[i].Push(char.Parse(line.Substring(1 + i + i * 3, 1)));
+                        break;
+                    }
+                    for (int i = 0; i < amountOfCrates; i++)
+                    {
+                        bool isValue = char.Parse(line.Substring(i + i * 3, 1)) == '[';
+                        if (isValue)
+                        {
+                            reversedCrateStacks[i].Push(char.Parse(line.Substring(1 + i + i * 3, 1)));
+                        }
                     }
                 }
+                while ((line = reader.ReadLine()) != null);
+                for (int i = 0; i < amountOfCrates; i++)
+                {
+                    while (reversedCrateStacks[i].Count > 0)
+                    {
+                        crateStacks[i].Push(reversedCrateStacks[i].Pop());
+                    }
+                }
+            }
+            Regex actionRegex = new Regex(@"move ([0-9]+) from ([0-9]+) to ([0-9]+)",
+          RegexOptions.Compiled);
+            while ((line = reader.ReadLine()) != null)
+            {
+                Match action = actionRegex.Match(line);
+                if (action.Success)
+                {
+                    GroupCollection actionGroup = action.Groups;
+                    int amountToMove = int.Parse(actionGroup[1].Value);
+                    int fromIndex = int.Parse(actionGroup[2].Value) - 1;
+                    int toIndex = int.Parse(actionGroup[3].Value) - 1;
+                    for (int i = 0; i < amountToMove; i++)
+                    {
+                        if (crateStacks[fromIndex].TryPop(out char result))
+                        {
+                            crateStacks[toIndex].Push(result);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < amountOfCrates; i++)
+            {
+                Console.Write(crateStacks[i].Peek());
             }
         }
         public static void Part2()
