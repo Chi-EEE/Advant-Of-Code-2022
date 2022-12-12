b35a0a897f84a6a81c2bc7b3ce161bb6f0bcd569
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2022-12-12 14:25:41 +0000

    Completed Day 5 - Part 2

--- Advant-Of-Code-2022/Advant-Of-Code-2022/Day5.cs
+++ Advant-Of-Code-2022/Advant-Of-Code-2022/Day5.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace Advant_Of_Code_2022
 {
     internal class Day5
     {
+        static readonly
+            Regex ACTION_REGEX = new Regex(@"move ([0-9]+) from ([0-9]+) to ([0-9]+)",
+          RegexOptions.Compiled);
         public static void Part1()
         {
             StreamReader reader = File.OpenText("Inputs/Day5.txt");
@@ -50,11 +54,9 @@ namespace Advant_Of_Code_2022
                     }
                 }
             }
-            Regex actionRegex = new Regex(@"move ([0-9]+) from ([0-9]+) to ([0-9]+)",
-          RegexOptions.Compiled);
             while ((line = reader.ReadLine()) != null)
             {
-                Match action = actionRegex.Match(line);
+                Match action = ACTION_REGEX.Match(line);
                 if (action.Success)
                 {
                     GroupCollection actionGroup = action.Groups;
@@ -80,6 +82,65 @@ namespace Advant_Of_Code_2022
             }
         }
         public static void Part2()
-        { }
+        {
+            StreamReader reader = File.OpenText("Inputs/Day5.txt");
+            string line = reader.ReadLine();
+            int amountOfCrates = (line.Length + 1) / 4;
+            List<List<char>> crateStacks = new();
+            for (int i = 0; i < amountOfCrates; i++)
+            {
+                crateStacks.Add(new());
+            }
+            {
+                List<Stack<char>> reversedCrateStacks = new();
+                for (int i = 0; i < amountOfCrates; i++)
+                {
+                    reversedCrateStacks.Add(new());
+                }
+                do
+                {
+                    if (line.Length == 0) // This is to separate the crates from the actions
+                    {
+                        break;
+                    }
+                    for (int i = 0; i < amountOfCrates; i++)
+                    {
+                        bool isValue = char.Parse(line.Substring(i + i * 3, 1)) == '[';
+                        if (isValue)
+                        {
+                            reversedCrateStacks[i].Push(char.Parse(line.Substring(1 + i + i * 3, 1)));
+                        }
+                    }
+                }
+                while ((line = reader.ReadLine()) != null);
+                for (int i = 0; i < amountOfCrates; i++)
+                {
+                    while (reversedCrateStacks[i].Count > 0)
+                    {
+                        crateStacks[i].Add(reversedCrateStacks[i].Pop());
+                    }
+                }
+            }
+            while ((line = reader.ReadLine()) != null)
+            {
+                Match action = ACTION_REGEX.Match(line);
+                if (action.Success)
+                {
+                    GroupCollection actionGroup = action.Groups;
+                    int amountToMove = int.Parse(actionGroup[1].Value);
+                    int fromIndex = int.Parse(actionGroup[2].Value) - 1;
+                    int toIndex = int.Parse(actionGroup[3].Value) - 1;
+                    int startCrateIndex = Math.Max(0, crateStacks[fromIndex].Count - amountToMove);
+                    int endCrateCount = amountToMove;
+                    List<char> range = crateStacks[fromIndex].GetRange(startCrateIndex, endCrateCount);
+                    crateStacks[fromIndex].RemoveRange(startCrateIndex, endCrateCount);
+                    crateStacks[toIndex].AddRange(range);
+                }
+            }
+            for (int i = 0; i < amountOfCrates; i++)
+            {
+                Console.Write(crateStacks[i][crateStacks[i].Count - 1]);
+            }
+        }
     }
 }
